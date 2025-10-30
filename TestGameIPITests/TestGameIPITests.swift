1e8574983ce784cf2d1419f06ec8a09e17fd692a
Jeffrey He <jdhe1120@Jeffreys-MacBook-Air.local>
2025-10-29 21:11:36 -0400

    first commit

---
+++ TestGameIPITests/TestGameIPITests.swift
@@ -0,0 +1,17 @@
+//
+//  TestGameIPITests.swift
+//  TestGameIPITests
+//
+//  Created by Jeffrey He on 10/28/25.
+//
+
+import Testing
+@testable import TestGameIPI
+
+struct TestGameIPITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
